3e953f7ea42d023ab2af31bd221e527ffa75dda2
Christinamawel <christinamawel@gmail.com>
2021-10-18 11:13:26 -0700

    add html and first functions
     Co-authored-by: Matt Casperson<mc.casperson@gmail.com>
    o-authored-by: Matt Casperson<mc.casperson@gmail.com>
    >
    Co-authored-by: Drew Henderson <drewhendersonemail@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+const literToGal = function (liters) {
+  return liters * .264172
+}
+
+const galtoLiter = function (gal) {
+  return gal * 3.78541
+}
+
+const cupToLiter = function (cups) {
+  return cups / 4.22675
+}
+
